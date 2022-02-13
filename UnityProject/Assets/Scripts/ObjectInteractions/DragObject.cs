8270463988c9150cc058318b2663f0eb3e90b8e2
R-Raykov <radoslav_raykov@abv.bg>
2022-02-14 01:23:31 +0200

    Added chair drag

--- UnityProject/Assets/Scripts/ObjectInteractions/DragObject.cs
+++ UnityProject/Assets/Scripts/ObjectInteractions/DragObject.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClothObject : InteractableObject
+public class DragObject : InteractableObject
 {
     private Vector3 startPos;
-    private Vector3 startScale;
+    private Quaternion startRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.localPosition;
-        startScale = transform.localScale;
+        startRotation = transform.localRotation;
     }
 
     protected override IEnumerator Animate()
@@ -19,12 +19,16 @@ public class ClothObject : InteractableObject
         float _currentTime = 0.0f;
 
         Vector3 _finalPosition = startPos;
-        Vector3 _finalScale = startScale;
+        Quaternion _finalRotation = startRotation;
 
         if (!isOpen)
         {
-            _finalPosition.x = 0.325f;
-            _finalScale.x = startScale.x / 4.0f;
+            Renderer _renderer = GetComponent<Renderer>();
+
+            _finalPosition = Vector3.zero;
+            _finalPosition.z = _renderer.bounds.extents.z;
+
+            _finalRotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
         }
 
         isOpen = !isOpen;
@@ -32,7 +36,7 @@ public class ClothObject : InteractableObject
         while (_currentTime < animationDuration)
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, _finalPosition, _currentTime / animationDuration);
-            transform.localScale = Vector3.Lerp(transform.localScale, _finalScale, _currentTime / animationDuration);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, _finalRotation, _currentTime / animationDuration);
 
             _currentTime += Time.deltaTime;
 
